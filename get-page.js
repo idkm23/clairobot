91ea0caf5590c876bc025a7b493968ce694a6ef7
gjakers <36972988+gjakers@users.noreply.github.com>
2019-02-06 10:19:23 -0500

    Update get-page.js

--- get-page.js
+++ get-page.js
@@ -14,7 +14,7 @@ const options = {
   headers: {
     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) " +
                   "AppleWebKit/537.36 (KHTML, like Gecko) " +
-                  "Chrome/62.0.3202.94 " +"
+                  "Chrome/62.0.3202.94 " +
                   "Safari/537.36"
   }
 };
