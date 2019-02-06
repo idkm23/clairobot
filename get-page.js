906eff4120b4e71b69f1819cbdcde28eba2e6650
gjakers <36972988+gjakers@users.noreply.github.com>
2019-02-06 10:19:09 -0500

    make pretty

--- get-page.js
+++ get-page.js
@@ -12,11 +12,10 @@ const r = new snoowrap( {
 const options = {
   url: "https://soundcloud.com/ccottrill",
   headers: {
-    'User-Agent': "Mozilla/5.0 " +
-                  "(Macintosh; Intel Mac OS X 10_12_6) " +
-                  "AppleWebKit/537.36 " + 
-                  "(KHTML, like Gecko) " +
-                  "Chrome/62.0.3202.94 Safari/537.36"
+    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) " +
+                  "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                  "Chrome/62.0.3202.94 " +"
+                  "Safari/537.36"
   }
 };
 
