c4c508ef6ab9d194615e9be1c68b49acbd40b1f2
gjakers <36972988+gjakers@users.noreply.github.com>
2019-02-05 21:32:07 -0500

    Update get-page.js

--- get-page.js
+++ get-page.js
@@ -1,13 +1,42 @@
-const https = require("https");
+const request  = require('request');
+const cheerio  = require('cheerio');
+const snoowrap = require('snoowrap');
 
-https.get('https://soundcloud.com/ccottrill/tracks', (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
+const r = new snoowrap( {
+  userAgent:     'clairobot',
+  clientId:      process.env.client_id,
+  clientSecret:  process.env.client_secret,
+  refresh_token: process.env.refresh_token
+} )
 
-  res.on('data', (d) => {
-    process.stdout.write(d);
+const options = {
+  url: "https://soundcloud.com/ccottrill",
+  headers: {
+    'User-Agent': "Mozilla/5.0 " +
+                  "(Macintosh; Intel Mac OS X 10_12_6) " +
+                  "AppleWebKit/537.36 " + 
+                  "(KHTML, like Gecko) " +
+                  "Chrome/62.0.3202.94 Safari/537.36"
+  }
+};
+
+request(options, function(err, response, body) {
+  const $ = cheerio.load(body.replace(/noscript/g, 'div' ));
+  
+  $('.audible').each( function(i, elem) {
+    var time  = $(elem).children('time').text();
+    var title = $( $(elem).children('h2').html() ).attr('href');
+    var url   = "https://soundcloud.com" + title;
+    title = title.replace(/\/ccottrill\//i, '').replace(/-/g, ' ');
+      
+    console.log(title, '  ', url );
+    
+    if( (Date.now() - Date.parse(time)) <= 600000 ) {
+      console.log("Creating post for [", title, "]\n");
+      r.getSubreddit('pizza_friends').submitLink( {
+        title: title,
+        url: url
+      })
+    }
   });
-
-}).on('error', (e) => {
-  console.error(e);
 });
